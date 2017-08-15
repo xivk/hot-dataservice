b05c8408c15f42c2a467ecf45aaa79dbc969221d
Ben Abelshausen <ben@osmsharp.com>
2017-08-15 20:35:18 +0100

    Finished a new version of this stuff.

---
+++ src/hotdataservice/Controllers/DataController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using OsmSharp.Streams;
+using hotdataservice.Tiles;
+
+namespace hotdataservice.Controllers
+{
+    [Route("[controller]")]
+    public class DataController : Controller
+    {
+        // GET data/{x}/{y}/{z}/{file}
+        [HttpGet("{x}/{y}/{z}/{file}")]
+        public IActionResult Get(int x, int y, int z, string file)
+        {
+            var completeFilePath = Path.Combine(Startup.DataFolder, file);
+            var fileInfo = new FileInfo(completeFilePath);
+            if (!fileInfo.Exists)
+            {
+                return new NotFoundResult();
+            }
+
+            var resultStream = new MemoryStream();
+            using (var stream = fileInfo.OpenRead())
+            {
+                OsmStreamSource source;
+                if (fileInfo.Name.EndsWith(".osm.pbf"))
+                {
+                    source = new PBFOsmStreamSource(stream);
+                }
+                else if (fileInfo.Name.EndsWith(".osm"))
+                {
+                    source = new XmlOsmStreamSource(stream);
+                }
+                else
+                {
+                    return new NotFoundResult();
+                }
+
+                // parse the zoom, x and y.
+                var tile = new Tile(x, y, z);
+                tile = tile.InvertY();
+                var filter = source.FilterBox(tile.Left, tile.Top, tile.Right, tile.Bottom,
+                    true);
+
+                var target = new XmlOsmStreamTarget(resultStream);
+                target.RegisterSource(filter);
+                target.Pull();
+
+                resultStream.Seek(0, SeekOrigin.Begin);
+            }
+            return new FileStreamResult(resultStream, "text/xml");
+        }
+    }
+}
---
+++ src/hotdataservice/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace hotdataservice
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            Startup.DataFolder = this.Configuration["DataFolder"];
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public static string DataFolder { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
